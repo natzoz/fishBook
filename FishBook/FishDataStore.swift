f04541b1e34e133f4eba629748513ecad0491d85
Sammy <cs-488-01@maclabcs20.lclark.edu>
2023-02-28 15:10:53 -0800

    deleted comment

--- FishBook/FishDataStore.swift
+++ FishBook/FishDataStore.swift
@@ -110,25 +110,4 @@ class FishDataStore {
         return fishes
     }
     
-//    func getFish(fishId: Int64) -> Fish? {
-//        var fish: Fish = Fish(id: fishId, commonName: "", scientificName: "", group: "", family: "", habitat: "", occurrence: "", description: "")
-//
-//        guard let database = db else {return nil}
-//
-//        let filter = self.fishes.filter(id == fishId)
-//        do {
-//            for f in try database.prepare(filter) {
-//                fish.commonName = f[commonName]
-//                fish.scientificName = f[scientificName]
-//                fish.group = f[group]
-//                fish.family = f[family]
-//                fish.habitat = f[habitat]
-//                fish.occurrence = f[occurrence]
-//                fish.description = f[description]
-//            }
-//        } catch {
-//            print(error)
-//        }
-//        return fish
-//    }
 }
