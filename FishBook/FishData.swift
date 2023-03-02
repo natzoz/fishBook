6a6a1ef0e6759140d7be197cff91d8bc5efb8490
natalie <nataliezoz@lclark.edu>
2023-03-02 13:03:40 -0800

    Created FamilyPage to show list of fish families

--- FishBook/FishData.swift
+++ FishBook/FishData.swift
@@ -2,11 +2,13 @@ import Foundation
 
 class FishData : ObservableObject{
     @Published var fishes: [Fish]
+    @Published var families: [String]
     
-    init(fishes: [Fish] = []) {
+    init(fishes: [Fish] = [], families: [String] = []) {
         self.fishes = fishes
+        self.families = families
     }
 }
 
-let allFishData = FishData(fishes: FishDataStore.share.getAllFish())
+let allFishData = FishData(fishes: FishDataStore.share.getAllFish(), families: FishDataStore.share.getAllFamilies())
 let testFishData = FishData(fishes: testData)
