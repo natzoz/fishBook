16a871462aeb2695fc9f1e38ea008699836413c5
Berto Gonzalez <eriberto@lclark.edu>
2023-03-14 15:30:26 -0700

    finished homepage

--- FishBook/HomePage.swift
+++ FishBook/HomePage.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct Frontview: View {
     @State private var randfish = false
     @State private var fishlist = false
+    @State private var ishidden = false
     var body: some View{
         return Group{
             if randfish {
@@ -18,31 +19,71 @@ struct Frontview: View {
                     FishDetailPage(fish: allFishData.fishes.randomElement()!)
                     Button("Go Back") {
                         randfish.toggle()
-                    }                }
-            }else{
-                ZStack{
-                    Color.orange.ignoresSafeArea()
-                    
-                    VStack{
-                        
-                        
                     }
-                
-                VStack{
-                    HStack{
-                        Spacer()
-                        Button("Random fish") {
-                            randfish.toggle()
-                        }
-                        Spacer()
-                        Button("Explore our FishBook") {
-                            fishlist.toggle()
-                        }
-                        Spacer()
-                    }
-                    .buttonStyle(.bordered)
                 }
-            }
+            }else if fishlist{
+                VStack{
+                    FishListPage(fishData: allFishData)
+                    Button("HomePage") {
+                        fishlist.toggle()
+                    }
+                }
+            }else{
+                Spacer()
+                VStack{
+                    Text("FishBook").font(.title)
+                    ImageSlider()
+                        .frame(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height/3)
+                    
+                        VStack{
+                            Text("Welcome to the world of FISH!!!")
+                            Text("gulp gulp gulp").font(.caption)
+                        }
+                    
+                    Spacer()
+                    HStack(alignment: .top){
+                        VStack(alignment: .leading){
+                            Text("Developed By:").bold()
+                            VStack(alignment: .leading){
+                                Text("Berto Gonzalez")
+                                Text("Sammy Gonzalez")
+                                Text("Kendal Jones")
+                                Text("Quinn Tonelli")
+                                Text("Natalie Zoz")
+                            }.padding(.leading, 8.0)
+                        }
+                        
+                        VStack(alignment: .leading){
+                            Text("Data Collector:").bold()
+                            Text("Kenneth Clifton")
+                                .padding(.leading, 8.0)
+            
+                            Text("Created For:").bold()
+                            VStack{
+                                Text("Peter Drake")
+                                Text("CS-488-01 Sof. Dev.")
+                            }.padding(.leading, 8)
+                        }
+                    }.padding(.trailing, 100)
+                    Spacer()
+                    
+                    Text("Here fish are friends, NOT FOOD").font(.callout)
+                    Spacer()
+                    
+                    VStack(alignment: .leading){
+                        HStack{
+                            Spacer()
+                            Button("Explore our FishBook") {
+                                fishlist.toggle()
+                            }
+                            Spacer()
+                            Button("Random Fish") {
+                                randfish.toggle()
+                            }
+                            Spacer()
+                        }.buttonStyle( .bordered)
+                    }
+                }
             }
         }
     }
@@ -53,3 +94,6 @@ struct HomePage_Previews: PreviewProvider {
         Frontview()
     }
 }
+
+
+
